c90f81a11b6f5e48739fd8d4c78671aac599290a
otengkwame <developerkwame@gmail.com>
2025-12-03 13:06:43 +0000

    chore: update profiler.php config file
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- config/profiler.php
+++ config/profiler.php
@@ -13,3 +13,15 @@ defined('COREPATH') or exit('No direct script access allowed');
 |
 */
 
+$config['benchmarks']           = true;
+$config['config']               = true;
+$config['controller_info']      = true;
+$config['get']                  = true;
+$config['http_headers']         = true;
+$config['memory_usage']         = true;
+$config['post']                 = true;
+$config['queries']              = true;
+$config['eloquent']             = false;
+$config['uri_string']           = true;
+$config['view_data']            = true;
+$config['query_toggle_count']   = 50;
