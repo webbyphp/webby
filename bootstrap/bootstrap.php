7aa65604fe6675605bfc7d2947ca8f5eb77661bf
otengkwame <developerkwame@gmail.com>
2025-12-03 11:44:53 +0000

    refactor: bootstraping files
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,42 +1,5 @@
 <?php
 
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2018, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package    CodeIgniter
- * @author    EllisLab Dev Team
- * @copyright    Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright    Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
- * @license    http://opensource.org/licenses/MIT    MIT License
- * @link    https://codeigniter.com
- * @since    Version 1.0.0
- * @filesource
- */
-
 /*
  * --------------------------------------------------------------------
  * DEFAULT CONTROLLER
@@ -259,7 +222,8 @@ define('CONTROLLERPATH', APPROOT . 'Controllers' . DIRECTORY_SEPARATOR);
 define('THIRDPARTYPATH', APPROOT . 'ThirdParty' . DIRECTORY_SEPARATOR);
 
 // Name of the folder that has the "codeigniter framework"
-define('SYSDIR',
+define(
+    'SYSDIR',
     trim(
         strrchr(
             trim(BASEPATH, DIRECTORY_SEPARATOR),
@@ -272,7 +236,7 @@ define('SYSDIR',
 // This used to be the "application" folder for codeigniter
 // The path to "core" folder
 if (is_dir($core_directory)) {
-    
+
     if (($_temp = realpath($core_directory)) !== false) {
         $core_directory = $_temp;
     }
@@ -340,7 +304,7 @@ if (is_dir($upload_directory)) {
 
     if (!is_dir(CIPATH . $upload_directory . DIRECTORY_SEPARATOR)) {
         header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'Your uploads data folder path does not appear to be set correctly. Please open the core/paths.php file and set a correct path on line '. $upload_directory_line;
+        echo 'Your uploads data folder path does not appear to be set correctly. Please open the core/paths.php file and set a correct path on line ' . $upload_directory_line;
         exit(3); // EXIT_CONFIG
     }
 
