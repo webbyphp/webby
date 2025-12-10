7f2c5e762a45a671ea0d59e3979cc0127d53280a
otengkwame <developerkwame@gmail.com>
2025-12-10 10:13:47 +0000

    chore: add markdown config file
    Signed-off-by: otengkwame <developerkwame@gmail.com>

---
+++ config/markdown.php
@@ -0,0 +1,15 @@
+<?php
+defined('COREPATH') or exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Markdown Parser Configuration
+|--------------------------------------------------------------------------
+|
+| You can change the cache path or disable caching completely.
+| By default, cache lives in writable/cache/markdown/
+|
+*/
+
+$config['markdown_cache_path'] = WRITABLEPATH . 'cache/markdown/';
+$config['markdown_cache_enabled'] = true;
