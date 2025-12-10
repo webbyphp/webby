850891cc8ea63c1ea8a071d3ba1819cfe6a25531
otengkwame <developerkwame@gmail.com>
2025-12-10 10:32:10 +0000

    docs: update hooks config comments
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- config/hooks.php
+++ config/hooks.php
@@ -6,9 +6,11 @@ defined('COREPATH') or exit('No direct script access allowed');
 | Hooks
 | -------------------------------------------------------------------------
 | This file lets you define "hooks" to extend Webby
-| without hacking the core files of CodeIgniter.  
+| without hacking the core files of the framework.  
 | Please see the user guide for info:
 |
-|	https://codeigniter.com/userguide3/general/hooks.html
+| You can visit https://codeigniter.com/userguide3/general/hooks.html
+| to get more understanding on how to use hooks the CI3 way and visit
+| https://webbyphp.top/docs/extending/hooks for new ways of using it. 
 |
 */
