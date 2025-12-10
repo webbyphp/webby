b83af653df34f08708e16f1a8047e396b131a3d7
otengkwame <developerkwame@gmail.com>
2025-12-10 03:11:55 +0000

    chore: update error display files
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -5,113 +5,69 @@
 
 <head>
 	<meta charset="utf-8">
-	<title>Error</title>
-	<style>
-		.w-div-shadow {
-			box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-			margin-top: 50px;
-		}
-
-		.w-webby-color {
-			color: #6d00cc;
-		}
-
-		.w-flex-center {
-			align-items: center;
-			display: flex;
-			justify-content: center;
-		}
-
-		.w-position-r {
-			position: relative;
-		}
-
-		.w-code-error {
-			border-right: 3px solid;
-			font-size: 55px;
-			padding: 0 10px 0 10px;
-			text-align: center;
-		}
-
-		.w-message-error {
-			font-size: 40px;
-			text-align: center;
-		}
-
-		.w-text-center {
-			text-align: center;
-		}
-
-		.w-button {
-			font-family: 'Montserrat', 'Arial', sans-serif;
-			display: inline-block;
-			font-size: 1.2em;
-			font-weight: 600;
-			padding: 1em 2em;
-			margin-top: 1em;
-			margin-bottom: 60px;
-			-webkit-appearance: none;
-			appearance: none;
-			background-color: #48049c;
-			color: #e3f6fa;
-			border-radius: 4px;
-			border: none;
-			cursor: pointer;
-			position: relative;
-			transition: transform ease-in 0.1s, box-shadow ease-in 0.25s;
-			box-shadow: 0 2px 25px rgba(204, 200, 180, 0.5);
-		}
-
-		.w-button:hover {
-			background-color: #5b08c2;
+	<title>404 Page Not Found</title>
+	<style type="text/css">
+		::selection {
+			background-color: #E13300;
 			color: white;
 		}
 
-		.w-button:focus {
-			outline: 0;
+		::-moz-selection {
+			background-color: #E13300;
+			color: white;
 		}
 
-		.w-button:before,
-		.w-button:after {
-			position: absolute;
-			content: '';
+		body {
+			background-color: #fff;
+			margin: 40px;
+			font: 13px/20px normal Helvetica, Arial, sans-serif;
+			color: #4F5155;
+		}
+
+		a {
+			color: #003399;
+			background-color: transparent;
+			font-weight: normal;
+		}
+
+		h1 {
+			color: #444;
+			background-color: transparent;
+			border-bottom: 1px solid #D0D0D0;
+			font-size: 19px;
+			font-weight: normal;
+			margin: 0 0 14px 0;
+			padding: 14px 15px 10px 15px;
+		}
+
+		code {
+			font-family: Consolas, Monaco, Courier New, Courier, monospace;
+			font-size: 12px;
+			background-color: #f9f9f9;
+			border: 1px solid #D0D0D0;
+			color: #002166;
 			display: block;
-			width: 140%;
-			height: 100%;
-			left: -20%;
-			z-index: -1000;
+			margin: 14px 0 14px 0;
+			padding: 12px 10px 12px 10px;
+		}
+
+		#container {
+			margin: 10px;
+			border: 1px solid #D0D0D0;
+			box-shadow: 0 0 8px #D0D0D0;
+		}
+
+		p {
+			margin: 12px 15px 12px 15px;
 		}
 	</style>
 </head>
 
 <body>
-	<div class="w-div-shadow" style="
-        border:2px solid #48049c;
-        padding:20px;
-        margin:100px 10px 10px 10px;
-    ">
-		<div class="w-flex-center w-position-r full-height">
-			<div class="w-code-error" style="color: red;">
-				<?php
-				http_response_code(404);
-				echo http_response_code();
-				?>
-			</div>
-
-			<div class="w-message-error w-webby-color" style="padding: 10px;">
-				NOT FOUND
-			</div><br>
-		</div>
-		<div style="text-align: center;">
-			<h2><code><?php echo ucwords($heading); ?></code></h2>
-			<code><?php echo $message; ?></code>
-		</div>
-
-		<div class="w-text-center" style="margin-top: 40px;">
-			<a class="w-button" href="javascript:window.history.go(-1);">GO BACK</a>
-		</div>
+	<div id="container">
+		<h1><?php echo $heading; ?></h1>
+		<?php echo $message; ?>
 	</div>
-
 </body>
 
 </html>
--- app/Views/errors/html/error_general.php
+++ app/Views/errors/html/error_general.php
@@ -6,64 +6,68 @@
 <head>
 	<meta charset="utf-8">
 	<title>Error</title>
-	<style>
-		.w-div-shadow {
-			box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-			margin-top: 50px;
+	<style type="text/css">
+		::selection {
+			background-color: #E13300;
+			color: white;
 		}
 
-		.w-webby-color {
-			color: #6d00cc;
+		::-moz-selection {
+			background-color: #E13300;
+			color: white;
 		}
 
-		.w-flex-center {
-			align-items: center;
-			display: flex;
-			justify-content: center;
+		body {
+			background-color: #fff;
+			margin: 40px;
+			font: 13px/20px normal Helvetica, Arial, sans-serif;
+			color: #4F5155;
 		}
 
-		.w-position-r {
-			position: relative;
+		a {
+			color: #003399;
+			background-color: transparent;
+			font-weight: normal;
 		}
 
-		.w-code-error {
-			border-right: 3px solid;
-			font-size: 55px;
-			padding: 0 10px 0 10px;
-			text-align: center;
+		h1 {
+			color: #444;
+			background-color: transparent;
+			border-bottom: 1px solid #D0D0D0;
+			font-size: 19px;
+			font-weight: normal;
+			margin: 0 0 14px 0;
+			padding: 14px 15px 10px 15px;
 		}
 
-		.w-message-error {
-			font-size: 40px;
-			text-align: center;
+		code {
+			font-family: Consolas, Monaco, Courier New, Courier, monospace;
+			font-size: 12px;
+			background-color: #f9f9f9;
+			border: 1px solid #D0D0D0;
+			color: #002166;
+			display: block;
+			margin: 14px 0 14px 0;
+			padding: 12px 10px 12px 10px;
+		}
+
+		#container {
+			margin: 10px;
+			border: 1px solid #D0D0D0;
+			box-shadow: 0 0 8px #D0D0D0;
+		}
+
+		p {
+			margin: 12px 15px 12px 15px;
 		}
 	</style>
 </head>
 
 <body>
-	<div class="w-div-shadow" style="
-        border:2px solid #48049c;
-        padding:20px;
-        margin:100px 10px 10px 10px;
-    ">
-		<div class="w-flex-center w-position-r full-height">
-			<div class="w-code-error" style="color: red;">
-				<?php 
-					set_status_header(500); 
-					echo http_response_code(500);
-				?>
-			</div>
-			
-			<div class="w-message-error w-webby-color" style="padding: 10px;">
-				INTERNAL SERVER ERROR
-			</div><br>
-		</div>
-		<div style="text-align: center;">
-			<h2><code><?php echo ucwords($heading); ?></code></h2>
-			<code><?php echo $message; ?></code>
-		</div>
+	<div id="container">
+		<h1><?php echo $heading; ?></h1>
+		<?php echo $message; ?>
 	</div>
-
 </body>
 
 </html>
